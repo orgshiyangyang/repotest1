0797d2fd9048a7f7cf68d4a27c58ed5462105c46
githubsyy0418 <1272988967@qq.com>
2019-04-18 19:00:19 +0800

    测试

---
+++ src/main/java/com/aaa/git/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.aaa.git.controller;
+
+/**
+ * fileName:TestController
+ * description:
+ * author:SYY
+ * createTime:2019/4/18 18:55
+ */
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
