f048c60a1fa6fa966a679e020d09ed7c98404d4e
githubsyy0418 <1272988967@qq.com>
2019-04-19 09:40:12 +0800

    修改了syy3

--- src/main/java/com/aaa/git/controller/TestController.java
+++ src/main/java/com/aaa/git/controller/TestController.java
@@ -9,6 +9,6 @@ package com.aaa.git.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("syy..");
-        System.out.println("添加打印");
+        System.out.println("添加打印1");
     }
 }
